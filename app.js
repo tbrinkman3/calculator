48a6d263efc0789c0ca185284a6c26f787688894
tbrinkman3 <tbrinkman3@gmail.com>
2020-10-19 21:37:10 -0500

    Added calculator functions

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const one = document.getElementById('one');
-const display = document.getElementById('display')
-
+const display = document.getElementById('display');
+const allClear = document.getElementById('all-clear');
+// One button
 one.addEventListener('click', function(){
     display.innerHTML = 1
 })
+//Clear button
+allClear.addEventListener('click', function(){
+    display.innerHTML = ''
+})
+
+//Calculator functions
+
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    if(a == 0){
+        return 0
+    } else if(b == 0){
+        return "Can't divide by zero"
+    }else{
+        return a/b
+    }
+}
+
